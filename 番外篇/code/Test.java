dfc25c081f112789364ac7d109b2d869160e2cac
小锅 <fxwlyx@163.com>
2021-08-08 14:55:31 +0800

    1

--- 番外篇/code/Test.java
+++ 番外篇/code/Test.java
@@ -1,7 +1,7 @@
 class Test {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello, world!");
+        System.out.println( "Hello, world!");
         System.exit(0);
     }
 }
